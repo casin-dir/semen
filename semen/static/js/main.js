dd6129d0846c134dbc70c84b7a8ccefc96d8736e
Casin <a.chashchin@ktsstudio.ru>
2017-06-17 02:50:33 +0300

    screen 1 start

--- semen/static/js/main.js
+++ semen/static/js/main.js
@@ -1,5 +1,22 @@
+// markup
+function fitFontSize() {
+    var currentWidth = window.innerWidth;
+    var currentHeight = window.innerHeight;
+    var initScale = 20;
+    var widthScale = currentWidth / 1440;
+    var heightScale = currentHeight / 860;
+    var currentScale = Math.min(widthScale, heightScale);
+    var fontSize = currentScale * initScale;
+    document.documentElement.style.fontSize = fontSize + 'px';
+}
+
+function initMarkup() {
+  fitFontSize();
+  window.addEventListener('resize', fitFontSize);
+}
+
 function ready() {
-    console.log('ready');
+    initMarkup();
 };
 
 document.addEventListener("DOMContentLoaded", ready);
