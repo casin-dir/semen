16294da9cc712364731d40cc320731a1a193a6a8
vladimirburash <vladimiburash@mail.ru>
2017-06-17 00:23:44 +0300

    add template for casin

--- semen_iphone/urls.py
+++ semen_iphone/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from semen.views import Landing
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', Landing.as_view()),
 ]
