658571e539a6d22c8e0c55fc196cf5330cbbd2f5
vladimirburash <vladimiburash@mail.ru>
2017-06-23 00:28:09 +0300

    add cost

--- semen/views.py
+++ semen/views.py
@@ -9,14 +9,28 @@ from django.views.decorators.csrf import csrf_exempt
 from crashes.models import Crash
 from devices.models import Device, Type
 from orders.models import Order
-from repairparts.models import RepairParts
 
+
+def total_cost(id):
+    cost = 0
+    crash = Crash.objects.get(id=id)
+    rps = crash.rps.all()
+    for rp in rps:
+        cost += rp.cost
+        pass
+    if crash.percent_cost != 0:
+        cost += cost * crash.percent_cost / 100
+    cost += crash.abs_cost
+    return cost
 
 class Landing(View):
     def get(self, request):
         context = {'list': []}
         Device.objects.all()
         types = Type.objects.all()
+
+
+
         for type in types:
             context['list'].append(
                 {'type': type,
@@ -30,31 +44,29 @@ class Landing(View):
                 for crash in crashes:
                     ch.append({'name': crash.name,
                                'id': crash.id,
-                               'url': crash.url})
-                con['models'].append({'name': device.name, 'id':device.id,  'crashes': ch})
+                               'url': crash.url,
+                               'cost': total_cost(crash.id)})
+                con['models'].append({'name': device.name, 'id': device.id, 'crashes': ch})
 
         return render(request, 'index.html', context)
 
-class NewOrder(View):
 
-    def post(self,request):
+class NewOrder(View):
+    def post(self, request):
         data = json.loads(self.request.body.decode('utf8'))
         crashes = []
-
-
 
         for crash in data['crashes']:
             crashes.append(Crash.objects.get(id=crash))
         total_cost = self.total(crashes)
         order = Order(
-            date = timezone.now(),
-            device = Crash.objects.get(id=1).device,
-            # crashes = crashes,
-            address = data['address'],
-            phone= data['phone'],
-            name = data['name'],
-            travel_time = 0,
-            total_cost = total_cost,
+            date=timezone.now(),
+            device=Crash.objects.get(id=1).device,
+            address=data['address'],
+            phone=data['phone'],
+            name=data['name'],
+            travel_time=0,
+            total_cost=total_cost,
             status='NEW'
 
         )
@@ -63,18 +75,9 @@ class NewOrder(View):
             order.crashes.add(crash)
         return HttpResponse(status=200)
 
-    def total(self,crashes):
+    def total(self, crashes):
         cost = 0
         for crash in crashes:
-            rps_cost = 0
-            cost += crash.abs_cost
-            crash = Crash.objects.get(id=crash.id)
-            rps = crash.rps.all()
-            for rp in rps:
-                rps_cost +=rp.cost
-            if crash.percent_cost != 0:
-                cost+=crash.percent_cost*rps_cost/100
-            cost+=rps_cost
+            cost +=total_cost(crash.id)
 
         return cost
-
