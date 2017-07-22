fc15d4754fb6936fa11ef40b878e05cd353456b2
root <root@goodmaster.moscow>
2017-07-22 22:39:38 +0000

    fix pro

--- semen_iphone/settings.py
+++ semen_iphone/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'goodmaster',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': 3306,
         'OPTIONS': {
